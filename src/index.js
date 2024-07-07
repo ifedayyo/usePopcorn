6001e57eaf51c4511e06fe84abcefa6b3dee0690
ifedayo <ifedayyo12@gmail.com>
2024-07-07 13:35:09 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 //import "./index.css";
 //import App from "./App";
+import AppTwo from "./AppTwo";
 
-import StarRating from "./StarRating";
+//import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
+    {/* <App />
+     */}
+
+    <AppTwo />
+
+    {/*{" "}
     <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
+    */}
   </React.StrictMode>
 );
